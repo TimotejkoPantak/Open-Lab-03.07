b02c54043a40c8c27d18d79d47136943f54b36d0
TimotejkoPantak <timopantakgit@gmail.com>
2020-10-18 11:55:50 +0200

    Implementing method that checks, if a cake can be divided fairly between people

--- Open-Lab-03.07/Divider.cs
+++ Open-Lab-03.07/Divider.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_03._07
     {
         public bool EqualSlices(int total, int people, int each)
         {
-            throw new NotImplementedException();
+            if (total >= people * each)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
         }
     }
 }
